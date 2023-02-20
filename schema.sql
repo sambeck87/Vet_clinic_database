fbfb1c1e29500e184af06bd2c9a11f0ada9d5e59
sambeck87 <sandro_mipb@hotmail.com>
2023-02-20 12:44:00 -0600

    Exercise to learn to create a simple data base Create a data base witch name be vet_clinic Create a table witch name be animals Add id, name, date of birth, scapes attempts, neutered, and wheight columns Populate the table Make the request queries

--- schema.sql
+++ schema.sql
@@ -1,5 +1,12 @@
 /* Database schema to keep the structure of entire database. */
+createdb vet_clinic
 
 CREATE TABLE animals (
-    name varchar(100)
+id INT GENERATED ALWAYS AS IDENTITY,
+name VARCHAR,
+date_of_birth DATE,
+escape_attempts INT,
+neutered BOOL,
+weight_kg DECIMAL
 );
+
