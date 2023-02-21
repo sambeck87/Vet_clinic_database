e1a6e7cbc13db3f915c702712057e634ac074301
sambeck87 <sandro_mipb@hotmail.com>
2023-02-20 21:48:37 -0600

    Save changes and push again

--- queries.sql
+++ queries.sql
@@ -77,32 +77,36 @@ SELECT * FROM animals;
 /*
  id |    name    | date_of_birth | escape_attempts | neutered | weight_kg |   species   
 ----+------------+---------------+-----------------+----------+-----------+-------------
+ 16 | Ditto      | 2022-05-14    |               4 | t        |        22 | unspecified
   1 | Agumon     | 2020-02-03    |               0 | t        |     10.23 | unspecified
   6 | Gabumon    | 2018-11-15    |               2 | t        |         8 | unspecified
   8 | Devimon    | 2017-05-12    |               5 | t        |        11 | unspecified
-  7 | Pikachu    | 2021-01-07    |               1 | f        |     15.04 | unspecified
- 10 | Plantmon   | 2021-11-15    |               2 | t        |      -5.7 | unspecified
- 11 | Squirtle   | 1993-04-02    |               3 | f        |    -12.13 | unspecified
- 12 | Angemon    | 2005-06-12    |               1 | t        |       -45 | unspecified
  13 | Boarmon    | 2005-06-07    |               7 | t        |      20.4 | unspecified
+  7 | Pikachu    | 2021-01-07    |               1 | f        |     15.04 | unspecified
  14 | Blossom    | 1998-10-13    |               3 | t        |        17 | unspecified
+ 10 | Plantmon   | 2021-11-15    |               2 | t        |      -5.7 | unspecified
+ 12 | Angemon    | 2005-06-12    |               1 | t        |       -45 | unspecified
+ 11 | Squirtle   | 1993-04-02    |               3 | f        |    -12.13 | unspecified
   9 | Charmander | 2020-02-08    |               0 | f        |       -11 | unspecified
+(11 filas)
 */
 ROLLBACK TO before_update;
 SELECT * FROM animals;
 /*
  id |    name    | date_of_birth | escape_attempts | neutered | weight_kg | species 
 ----+------------+---------------+-----------------+----------+-----------+---------
+ 16 | Ditto      | 2022-05-14    |               4 | t        |        22 | 
   1 | Agumon     | 2020-02-03    |               0 | t        |     10.23 | 
   6 | Gabumon    | 2018-11-15    |               2 | t        |         8 | 
   8 | Devimon    | 2017-05-12    |               5 | t        |        11 | 
-  7 | Pikachu    | 2021-01-07    |               1 | f        |     15.04 | 
- 10 | Plantmon   | 2021-11-15    |               2 | t        |      -5.7 | 
- 11 | Squirtle   | 1993-04-02    |               3 | f        |    -12.13 | 
- 12 | Angemon    | 2005-06-12    |               1 | t        |       -45 | 
  13 | Boarmon    | 2005-06-07    |               7 | t        |      20.4 | 
+  7 | Pikachu    | 2021-01-07    |               1 | f        |     15.04 | 
  14 | Blossom    | 1998-10-13    |               3 | t        |        17 | 
+ 10 | Plantmon   | 2021-11-15    |               2 | t        |      -5.7 | 
+ 12 | Angemon    | 2005-06-12    |               1 | t        |       -45 | 
+ 11 | Squirtle   | 1993-04-02    |               3 | f        |    -12.13 | 
   9 | Charmander | 2020-02-08    |               0 | f        |       -11 | 
+(11 filas)
 */
 COMMIT;
 
@@ -119,13 +123,15 @@ SELECT * FROM animals;
   1 | Agumon     | 2020-02-03    |               0 | t        |     10.23 | digimon
   6 | Gabumon    | 2018-11-15    |               2 | t        |         8 | digimon
   8 | Devimon    | 2017-05-12    |               5 | t        |        11 | digimon
+ 13 | Boarmon    | 2005-06-07    |               7 | t        |      20.4 | digimon
  10 | Plantmon   | 2021-11-15    |               2 | t        |      -5.7 | digimon
  12 | Angemon    | 2005-06-12    |               1 | t        |       -45 | digimon
- 13 | Boarmon    | 2005-06-07    |               7 | t        |      20.4 | digimon
+ 16 | Ditto      | 2022-05-14    |               4 | t        |        22 | pokemon
   7 | Pikachu    | 2021-01-07    |               1 | f        |     15.04 | pokemon
- 11 | Squirtle   | 1993-04-02    |               3 | f        |    -12.13 | pokemon
  14 | Blossom    | 1998-10-13    |               3 | t        |        17 | pokemon
+ 11 | Squirtle   | 1993-04-02    |               3 | f        |    -12.13 | pokemon
   9 | Charmander | 2020-02-08    |               0 | f        |       -11 | pokemon
+(11 filas)
 */
 
 /* delete all data and restore it */
@@ -148,25 +154,40 @@ SELECT * FROM animals;
   1 | Agumon     | 2020-02-03    |               0 | t        |     10.23 | digimon
   6 | Gabumon    | 2018-11-15    |               2 | t        |         8 | digimon
   8 | Devimon    | 2017-05-12    |               5 | t        |        11 | digimon
+ 13 | Boarmon    | 2005-06-07    |               7 | t        |      20.4 | digimon
  10 | Plantmon   | 2021-11-15    |               2 | t        |      -5.7 | digimon
  12 | Angemon    | 2005-06-12    |               1 | t        |       -45 | digimon
- 13 | Boarmon    | 2005-06-07    |               7 | t        |      20.4 | digimon
+ 16 | Ditto      | 2022-05-14    |               4 | t        |        22 | pokemon
   7 | Pikachu    | 2021-01-07    |               1 | f        |     15.04 | pokemon
- 11 | Squirtle   | 1993-04-02    |               3 | f        |    -12.13 | pokemon
  14 | Blossom    | 1998-10-13    |               3 | t        |        17 | pokemon
+ 11 | Squirtle   | 1993-04-02    |               3 | f        |    -12.13 | pokemon
   9 | Charmander | 2020-02-08    |               0 | f        |       -11 | pokemon
-(10 filas)
+(11 filas)
 */
 
 COMMIT;
 
 /* Change weight */
 
-COMMIT;
-COMMIT
-vet_clinic=# BEGIN;
-DELETE FROM animals WHERE date_of_birth>'Jan 1, 2022';
+BEGIN;
 SAVEPOINT before_delete_date;
+DELETE FROM animals WHERE date_of_birth>'Jan 1, 2022';
+SELECT * FROM animals;
+/*
+ id |    name    | date_of_birth | escape_attempts | neutered | weight_kg | species 
+----+------------+---------------+-----------------+----------+-----------+---------
+  1 | Agumon     | 2020-02-03    |               0 | t        |     10.23 | digimon
+  6 | Gabumon    | 2018-11-15    |               2 | t        |         8 | digimon
+  8 | Devimon    | 2017-05-12    |               5 | t        |        11 | digimon
+ 13 | Boarmon    | 2005-06-07    |               7 | t        |      20.4 | digimon
+  7 | Pikachu    | 2021-01-07    |               1 | f        |     15.04 | pokemon
+ 14 | Blossom    | 1998-10-13    |               3 | t        |        17 | pokemon
+ 10 | Plantmon   | 2021-11-15    |               2 | t        |       5.7 | digimon
+ 12 | Angemon    | 2005-06-12    |               1 | t        |        45 | digimon
+ 11 | Squirtle   | 1993-04-02    |               3 | f        |     12.13 | pokemon
+  9 | Charmander | 2020-02-08    |               0 | f        |        11 | pokemon
+(10 filas)
+*/
 UPDATE animals SET weight_kg=weight_kg*-1;
 SELECT * FROM animals;
 /*
@@ -175,14 +196,14 @@ SELECT * FROM animals;
   1 | Agumon     | 2020-02-03    |               0 | t        |    -10.23 | digimon
   6 | Gabumon    | 2018-11-15    |               2 | t        |        -8 | digimon
   8 | Devimon    | 2017-05-12    |               5 | t        |       -11 | digimon
+ 13 | Boarmon    | 2005-06-07    |               7 | t        |     -20.4 | digimon
  10 | Plantmon   | 2021-11-15    |               2 | t        |       5.7 | digimon
  12 | Angemon    | 2005-06-12    |               1 | t        |        45 | digimon
- 13 | Boarmon    | 2005-06-07    |               7 | t        |     -20.4 | digimon
   7 | Pikachu    | 2021-01-07    |               1 | f        |    -15.04 | pokemon
- 11 | Squirtle   | 1993-04-02    |               3 | f        |     12.13 | pokemon
  14 | Blossom    | 1998-10-13    |               3 | t        |       -17 | pokemon
+ 11 | Squirtle   | 1993-04-02    |               3 | f        |     12.13 | pokemon
   9 | Charmander | 2020-02-08    |               0 | f        |        11 | pokemon
-(10 filas)
+
 */
 
 ROLLBACK TO before_delete_date;
@@ -193,14 +214,14 @@ SELECT * FROM animals;
   1 | Agumon     | 2020-02-03    |               0 | t        |     10.23 | digimon
   6 | Gabumon    | 2018-11-15    |               2 | t        |         8 | digimon
   8 | Devimon    | 2017-05-12    |               5 | t        |        11 | digimon
+ 13 | Boarmon    | 2005-06-07    |               7 | t        |      20.4 | digimon
  10 | Plantmon   | 2021-11-15    |               2 | t        |      -5.7 | digimon
  12 | Angemon    | 2005-06-12    |               1 | t        |       -45 | digimon
- 13 | Boarmon    | 2005-06-07    |               7 | t        |      20.4 | digimon
+ 16 | Ditto      | 2022-05-14    |               4 | t        |        22 | pokemon
   7 | Pikachu    | 2021-01-07    |               1 | f        |     15.04 | pokemon
- 11 | Squirtle   | 1993-04-02    |               3 | f        |    -12.13 | pokemon
  14 | Blossom    | 1998-10-13    |               3 | t        |        17 | pokemon
+ 11 | Squirtle   | 1993-04-02    |               3 | f        |    -12.13 | pokemon
   9 | Charmander | 2020-02-08    |               0 | f        |       -11 | pokemon
-(10 filas)
 */
 
 UPDATE animals SET weight_kg=weight_kg*-1 
@@ -209,16 +230,17 @@ SELECT * FROM animals;
 /*
  id |    name    | date_of_birth | escape_attempts | neutered | weight_kg | species 
 ----+------------+---------------+-----------------+----------+-----------+---------
-  1 | Agumon     | 2020-02-03    |               0 | t        |     10.23 | digimon
-  6 | Gabumon    | 2018-11-15    |               2 | t        |         8 | digimon
-  8 | Devimon    | 2017-05-12    |               5 | t        |        11 | digimon
- 13 | Boarmon    | 2005-06-07    |               7 | t        |      20.4 | digimon
-  7 | Pikachu    | 2021-01-07    |               1 | f        |     15.04 | pokemon
- 14 | Blossom    | 1998-10-13    |               3 | t        |        17 | pokemon
  10 | Plantmon   | 2021-11-15    |               2 | t        |       5.7 | digimon
  12 | Angemon    | 2005-06-12    |               1 | t        |        45 | digimon
  11 | Squirtle   | 1993-04-02    |               3 | f        |     12.13 | pokemon
   9 | Charmander | 2020-02-08    |               0 | f        |        11 | pokemon
+  1 | Agumon     | 2020-02-03    |               0 | t        |     10.23 | digimon
+  6 | Gabumon    | 2018-11-15    |               2 | t        |         8 | digimon
+  8 | Devimon    | 2017-05-12    |               5 | t        |        11 | digimon
+ 13 | Boarmon    | 2005-06-07    |               7 | t        |      20.4 | digimon
+ 16 | Ditto      | 2022-05-14    |               4 | t        |        22 | pokemon
+  7 | Pikachu    | 2021-01-07    |               1 | f        |     15.04 | pokemon
+ 14 | Blossom    | 1998-10-13    |               3 | t        |        17 | pokemon
 */
 COMMIT;
 
@@ -229,8 +251,9 @@ SELECT COUNT(*) FROM animals;
 /*
  count 
 -------
-    10
+    11
 (1 fila)
+
 */
 
 /* How many animals have never tried to escape? */
@@ -241,24 +264,27 @@ WHERE escape_attempts=0;
 -------
      2
 (1 fila)
+
 */
 
 /* What is the average weight of animals? */
 SELECT AVG(weight_kg) FROM animals;
 /*
+         avg         
 ---------------------
- 15.5500000000000000
-(1 fila)*/
+ 16.1363636363636364
+(1 fila)
+*/
 
 /* Who escapes the most, neutered or not neutered animals? */
-SELECT neutered, SUM(escape_attempts) FROM animals 
+SELECT neutered, AVG(escape_attempts) as AVG_escape
+FROM animals
 GROUP BY neutered;
 /*
- neutered | sum 
-----------+-----
- f        |   4
- t        |  20
-(2 filas)
+ neutered |     avg_escape     
+----------+--------------------
+ f        | 1.3333333333333333
+ t        | 3.0000000000000000
 */
 
 /* What is the minimum and maximum weight of each type of animal? */
@@ -277,7 +303,7 @@ GROUP BY species;
 /*
  species | max 
 ---------+-----
- pokemon |  17
+ pokemon |  22
  digimon |  45
 (2 filas)
 */
