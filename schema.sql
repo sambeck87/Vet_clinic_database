17b43bd8aa5a639cbd6b6534cf8e6e525234a323
sambeck87 <sandro_mipb@hotmail.com>
2023-02-22 16:00:58 -0600

    Create tables for many to many queries Add vets table Add specializatios table Add visits table Add data to vets table Add data to specializations table Add data to visits table Make queries to answer the questions

--- schema.sql
+++ schema.sql
@@ -51,3 +51,26 @@ ALTER TABLE animals DROP species;
 ALTER TABLE animals ADD COLUMN species_id INT REFERENCES species(id);
 ALTER TABLE animals ADD COLUMN owner_id INT REFERENCES owners(id);
 
+/* ****************************************************************************************** */
+
+CREATE TABLE vets 
+( 
+  id INT GENERATED ALWAYS AS IDENTITY, 
+  name VARCHAR NOT NULL, 
+  age INT NOT NULL, 
+  date_of_graduation DATE NOT NULL, 
+  PRIMARY KEY(id)
+);
+
+CREATE TABLE specializations
+(
+  species_id INT REFERENCES species(id), 
+  vet_id INT REFERENCES vets(id)
+);
+
+CREATE TABLE visits
+(
+  animal_id INT REFERENCES animals(id),
+   vet_id INT REFERENCES vets(id),
+    visit_date DATE
+);
